ee9d2f68388ffc4701e7da34a85dedd88728f2e1
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-08-08 19:36:57 +0100

    Initial commit

---
+++ app/about/page.jsx
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="font-bold text-center text-4xl p-10">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa tenetur,
+        ea repellendus fugit sequi eveniet quam id veniam cum quae nemo dolorem
+        in a soluta dolorum rerum beatae vero ad. Lorem ipsum dolor sit amet
+        consectetur, adipisicing elit. Dolores quibusdam dolorum quas minima!
+        Deleniti veniam suscipit maxime voluptates itaque quae!lorem Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Labore, esse!
+      </p>
+    </div>
+  );
+};
+
+export default About;
